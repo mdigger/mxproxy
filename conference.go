048f935b4e22ede4cd76f94e4e4ae17ea6de7de2
Dmitry Sedykh <dmitrys@xyzrd.com>
2018-02-26 03:09:51 +0300

    ConferenceCreate

--- conference.go
+++ conference.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/mdigger/mx"
+import (
+	"encoding/xml"
+
+	"github.com/mdigger/mx"
+)
 
 // Conference описывает параметры конференции.
 type Conference struct {
@@ -16,4 +20,26 @@ type Conference struct {
 	DelOnOwnerLeave bool   `xml:"delOnOwnerLeave" json:"delOnOwnerLeave"`
 	Ws              string `xml:"ws" json:"ws"`
 	WsType          string `xml:"wsType" json:"wsType"`
+}
+
+// ConferenceCreate инициализирует новую конференцию.
+func (c *MXConn) ConferenceCreate(params *Conference) (*Conference, error) {
+	if _, err := c.SendWithResponse(&struct {
+		XMLName xml.Name `xml:"CreateConference"`
+		*Conference
+	}{
+		Conference: params,
+	}); err != nil {
+		return nil, err
+	}
+	err := c.HandleWait(func(resp *mx.Response) error {
+		if err := resp.Decode(params); err != nil {
+			return err
+		}
+		return mx.Stop
+	}, mx.ReadTimeout, "ConfAddEvent")
+	if err != nil {
+		return nil, err
+	}
+	return params, nil
 }
